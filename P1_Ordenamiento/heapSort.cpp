d943d177184007e3e8b389463c69d579a44be734
Jackson Merma <jmermap@unsa.edu.pe>
2021-10-30 17:34:00 -0500

    heapSort con datos genericos(uso de templates de un solo tipo)

--- P1_Ordenamiento/heapSort.cpp
+++ P1_Ordenamiento/heapSort.cpp
@@ -10,12 +10,15 @@ using namespace std;
 #define ll long long
 #define endl "\n"
 
-void swap(int arr[], int a, int b){
-	int aux=arr[a];
+template <class X>
+void swap(X arr[], int a, int b){
+	X aux=arr[a];
 	arr[a]=arr[b];
 	arr[b]=aux;
 }
-void heapify(int arr[], int idx, int max){
+
+template <class X>
+void heapify(X arr[], int idx, int max){
 	int lar=idx,left=2*idx+1,right=2*idx+2;
 
 	if(left<max&&arr[left]>arr[idx])
@@ -27,34 +30,37 @@ void heapify(int arr[], int idx, int max){
 		heapify(arr,lar,max);
 	}
 }
-void buildHeap(int arr[], int len){
+
+template <class X>
+void buildHeap(X arr[], int len){
 	for(int i=len/2-1;i>=0;i--)
 		heapify(arr, i, len);
 }
 
-void sort(int arr[], int len){
+template <class X>
+void sort(X arr[], int len){
 	buildHeap(arr,len);
 	for(int i=len-1;i>0;i--){
 		swap(arr,0,i);
 		heapify(arr,0,i);
-		for(int u=0;u<len;++u)
-			cout<<arr[u]<<" ";
-		//cout<<endl;
 	}
 }
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n;
-	cin>>n;
-	int arr[n];
-	for(int i=0;i<n;i++)
-		cin>>arr[i];
+	int arr1[] = {7,2,3,1,9,3,1,0};
+	string arr2[]={"flipo","hola","jack","b","random","cpp","x","holo"};
 
-	sort(arr,n);
-	for(int i=0;i<n;i++)
-		cout<<arr[i]<<" ";
+	sort(arr1,8);
+	sort(arr2,8);
+
+	for(int i=0;i<8;i++)
+		cout<<arr1[i]<<" ";
+	cout<<endl;
+	for(int i=0;i<8;i++)
+		cout<<arr2[i]<<" ";
+	cout<<endl;
 
 	return 0;
 }
