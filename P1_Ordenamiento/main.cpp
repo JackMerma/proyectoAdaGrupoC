16d4cb73b6a18b8b65ae96071c5c57cdc4f5f62e
Jackson Merma <jmermap@unsa.edu.pe>
2021-10-30 23:27:03 -0500

    funcion para la opcion 1 (ingreso de datos) y implementacion de una funcion general para procesar los algoritmos. Tambien se redefine entrada de datos

--- P1_Ordenamiento/main.cpp
+++ P1_Ordenamiento/main.cpp
@@ -14,23 +14,75 @@ using namespace std;
 #define endl "\n"
 
 template <class X>
-void procesar_algoritmo(int opt,X arr,int len){
+void procesar_algoritmo(X arr,int len){
+	//algoritmo a usar
+	int optAlgo;
+	printf("Algoritmo de ordenamiento a usar:\n1. Quick Sort\n2. Heap Sort\n3. Insertion Sort\nOpcion:");
+	scanf("%i",&optAlgo);
+
+
+	//prueba
+	for(int i=0;i<len;i++)
+		printf("%i ",arr[i]);
+
+	//algoritmos
+
+
+	printf("Funciona\n");
+
+	//Tiempo - salida
 
 }
 
+void opcion_1(){
+	int n;
+	printf("Cantidad de datos:");
+	scanf("%i",&n);
+
+	int optTipo;
+	printf("Tipo de datos:\n1. Letras\n2. Numeros\nOpcion:");
+	scanf("%i",&optTipo);
+
+	//manejo de malas entradas
+	if(optTipo<1||optTipo>2){
+		optTipo=1;
+		printf("Default: letras\n");
+	}
+
+	if(optTipo==1){													//Letras
+		string arr[n];
+		printf("Ingrese los datos separados por espacios:");
+
+		for(int i=0;i<n;i++)
+			scanf("%s",&arr[i]);
+
+		procesar_algoritmo(arr,n);
+
+
+
+	}else{																	//Numeros
+		int arr[n];
+		printf("Ingrese los datos separados por espacios:\n");
+
+		for(int i=0;i<n;i++)
+			scanf("%i",&arr[i]);
+
+		procesar_algoritmo(arr,n);
+	}
+}
 void opcion_2(){
-	ll n;
+	int n;
 	//los datos se generan aleatoriamente
 	printf("Advertencia: Los datos se generaran aleatoriamente (datos numericos)\n");
-	printf("Cantidad de datos:\n");
-	scanf("%cant",&n);
+	printf("Cantidad de datos:");
+	scanf("%i",&n);
 	n=abs(n);//si se ingresa un negativo
 
 	ll arr[n]={0};
 
 	ll a=1,b=0;
-	printf("Rangos(separada por espacios):\n");
-	scanf("%a %b", &a,&b);
+	printf("Rangos(separada por espacios):");
+	scanf("%i %i", &a,&b);
 
 	//para datos invalidos
 	ll minX=min(a,b),maxX=max(a,b);
@@ -41,13 +93,8 @@ void opcion_2(){
 	for(int i=0;i<n;i++)
 		arr[i]=rand()%(b-a+1)+a;
 
-	//algoritmo a usar
-	int optAlgo;
-	printf("Algoritmo de ordenamiento a usar:\n1. Quick Sort\n2. Heap Sort\n3. Insertion Sort\n");
-	scanf("%opt",&optAlgo);
+	procesar_algoritmo(arr,n);
 
-	procesar_algoritmo(optAlgo,arr,n);
-	
 }
 
 int main(){
@@ -58,9 +105,9 @@ int main(){
 
 	while(opt!=3){
 		printf("******** Menu ********\n1. Ingresar datos\n2. Generar datos\n3. Salir\n");
-		printf("Opcion:");scanf("%option",&opt);
+		printf("Opcion:");scanf("%i",&opt);
 		if(opt==1){
-
+			opcion_1();
 		}else if(opt==2){
 			opcion_2();
 		}else if(opt==3){
