1de7ae0b1279a639a0ff7ebefefc954b7faa988b
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-07 20:49:28 -0500

    metodo para convertir a escala de grises la imagen

--- P2_Recorrido_en_grafos/Image.cpp
+++ P2_Recorrido_en_grafos/Image.cpp
@@ -29,10 +29,32 @@ class Image{
 				cout<<"Error al cargar la imagen"<<endl;
 				exit(1);
 			}
-			stbi_image_free(image);
+			//stbi_image_free(image);
 		}
 		void write(){}
-		void convert_to_gray(){}
+		void convert_to_gray(){
+			size_t img_size=width*height*channels;
+			int gray_channels=channels==4?2:1;
+			size_t gray_img_size=width*height*gray_channels;
+			
+			//localizar memoria para la imagen de grises
+			unsigned char *gray_img=(unsigned char*)malloc(gray_img_size);
+
+			if(gray_img==NULL){
+				cout<<"No se encontro el recurso"<<endl;
+				exit(1);
+			}
+
+			for(unsigned char *p=image, *pg=gray_img; p!= image+img_size; p+=channels,pg+=gray_channels){
+				*pg = (uint8_t)((*p+*(p+1)+*(p+2))/3.0); //rgb
+				if(channels==4){
+					*(pg+1)=*(p+3);
+				}
+			}
+
+			this->channels=gray_channels;
+			this->image=gray_img;
+		}
 		friend string to_string(Image const& img){
 			return "width:"+to_string(img.width)+
 				"\nheight:"+to_string(img.height)+
@@ -46,6 +68,7 @@ int main(){
 
 	Image myImage;
 	myImage.read("img.jpg");
+	myImage.convert_to_gray();
 	cout<<to_string(myImage)<<endl;
 
 	return 0;
