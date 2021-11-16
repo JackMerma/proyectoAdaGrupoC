a3b21a52b486c32c418346162813677a0c10ccf4
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-16 00:42:21 -0500

    version 2 de busqueda con complejidad 1 para los ya visitados

--- P2_Recorrido_en_grafos/main.cpp
+++ P2_Recorrido_en_grafos/main.cpp
@@ -12,32 +12,33 @@ using namespace std;
 #define endl "\n"
 
 int RANGE=20;//+-10
-int conteo=0;
 vector<int> color_group;
-vector<int> posiciones_recorridas;
+int posiciones_recorridas[100000000]={0};//largo
 
-void dfs(unsigned char img[], int pos, int n, int color){
-	int color_actual=(int)img[pos];
+void dfs(Image img,int pos, int color){
+
+	unsigned char *data=img.image;
+	int width=img.width;
+	int height=img.height;
+
+	int color_actual=(int)data[pos];
 	if(color_actual<color-RANGE || color_actual>color+RANGE)//fuera de rango de pintado
 		return;
 
-	for(int i=0;i<posiciones_recorridas.size();i++)
-		if(pos==posiciones_recorridas[i]) return;
+	if(posiciones_recorridas[pos]==1) return;
 	//se agrega
-	posiciones_recorridas.push_back(pos);
-	cout<<pos<<endl;
-	conteo++;
+	posiciones_recorridas[pos]=1;
 	//cout<<(int)img[pos]<<" ";
 	//img[pos]=(unsigned char)color;//no es necesario para la primera pasada
 
-	if(pos-n>=0)//arriba
-		dfs(img,pos-n,n,color);
-	if(pos+n<n*n)//abajo
-		dfs(img,pos+n,n,color);
-	if((pos+1)%n!=0)//derecha
-		dfs(img,pos+1,n,color);
-	if(pos%n!=0)//izquierda
-		dfs(img,pos-1,n,color);
+	if(pos-width>=0)//arriba
+		dfs(img,pos-width,color);
+	if(pos+width<width*height)//abajo
+		dfs(img,pos+width,color);
+	if((pos+1)%width!=0)//derecha
+		dfs(img,pos+1,color);
+	if(pos%width!=0)//izquierda
+		dfs(img,pos-1,color);
 }
 
 int main(){
@@ -45,33 +46,23 @@ int main(){
 	cin.tie(0);
 
 	Image myImage;
-	cout<<"informacion ... "<<endl;
 	myImage.read("other.jpg");
-
-	cout<<to_string(myImage)<<endl;
 
 	//recorrido DFS
 	for(int i=0;i<myImage.height*myImage.width;i++){
-		bool en_rango=false;
 		int color=(int)myImage.image[i];
-		//consulta si ya esta dentro del rango de uno de los colores principales
-		for(int u=0;u<color_group.size();u++){
-			if(color>=color_group[u]-RANGE && color<= color_group[u]+RANGE){
-				en_rango=true;
-			}
-		}
-		//si no estuvo en rango de ninguno
-		if(en_rango==false){
+		if(posiciones_recorridas[i]==0){
 			color_group.push_back(color);//coloca al color principal
-			dfs(myImage.image, i,myImage.width, color);//hace dfs
-			posiciones_recorridas.clear();
+			dfs(myImage, i, color);//hace dfs
 		}
 	}
 
 	cout<<"grupos formados:"<<color_group.size()<<endl;
 	for(int i=0;i<color_group.size();i++)
 		cout<<color_group[i]<<" ";
-	cout<<endl<<"count:"<<conteo<<endl;
+	cout<<endl;
+	cout<<"informacion ... "<<endl;
+	cout<<to_string(myImage)<<endl;
 	//recorrido BFS
 
 
