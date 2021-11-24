3dc5875e27545211ade9b12dc0059ec8e9260b74
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-23 20:13:39 -0500

    lectura de imagenes con dos casos base

--- P2_Recorrido_en_grafos/main.cpp
+++ P2_Recorrido_en_grafos/main.cpp
@@ -2,7 +2,7 @@
  * @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
  * @created     : 07/11/2021
  * @filename    : Image
- * @description : Clase imagen que lee, escribe y convierte a escala de grises una imagen
+ * @description : Main principal, donde se ralizan procedimientos generales con las imagenes (lectura, escritura y recorridos)
  */
 #include <bits/stdc++.h>
 #include "dfs_bfs.cpp"//ya incluye a Image.cpp
@@ -15,18 +15,23 @@ int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	Image myImage;
-	myImage.read("other.jpg");
 
-	//recorrido DFS
-	make_dfs(myImage);
+	Image img1;
+	img1.read("imagenes/dory.jpg");
 
-	//recorrido BFS
-	make_bfs(myImage);
+	Image img2;
+	img2.read("imagenes/nemo.jpg");
 
-	informacion_dfs();
-	informacion_bfs();
+	//recorridos diferentes en imagenes diferentes
+	make_dfs(img1, 31);//25-30
+	make_bfs(img2, 60);//55-60-70
 
-	cout<<endl;
+
+	//salidas
+	img1.write("imagenes/dory2","jpg");
+	img2.write("imagenes/nemo2","jpg");
+
+	cout<<"Proceso terminado."<<endl;
+
 	return 0;
 }
