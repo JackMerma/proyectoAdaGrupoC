b78e37cfae4e2e286aeda7b99c4e9ff6c53a2a74
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-23 20:39:13 -0500

    implementacion de varios casos de prueba

--- P2_Recorrido_en_grafos/main.cpp
+++ P2_Recorrido_en_grafos/main.cpp
@@ -15,21 +15,63 @@ int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	/**
+	 * Casos en rangos de 20 - 40 y 60 (+-) 
+	 */
+	Image dory1,dory2,dory3;
+		//lectura
+	dory1.read("imagenes/dory.jpg");
+	dory2.read("imagenes/dory.jpg");
+	dory3.read("imagenes/dory.jpg");
 
-	Image img1;
-	img1.read("imagenes/dory.jpg");
+		//recorrido
+	make_dfs(dory1, 20);
+	make_dfs(dory2, 40);
+	make_dfs(dory3, 60);
 
-	Image img2;
-	img2.read("imagenes/nemo.jpg");
+		//escritura
+	dory1.write("imagenes/dory_20","jpg");
+	dory2.write("imagenes/dory_40","jpg");
+	dory3.write("imagenes/dory_60","jpg");
+		
+		//lectura
+	Image nemo1,nemo2,nemo3;
+	nemo1.read("imagenes/nemo.jpg");
+	nemo2.read("imagenes/nemo.jpg");
+	nemo3.read("imagenes/nemo.jpg");
+		
+		//recorrido
+	make_bfs(nemo1, 20);
+	make_bfs(nemo2, 40);
+	make_bfs(nemo3, 60);
 
-	//recorridos diferentes en imagenes diferentes
-	make_dfs(img1, 31);//25-30
-	make_bfs(img2, 60);//55-60-70
+		//escritura
+	nemo1.write("imagenes/nemo_20","jpg");
+	nemo2.write("imagenes/nemo_40","jpg");
+	nemo3.write("imagenes/nemo_60","jpg");
 
 
-	//salidas
-	img1.write("imagenes/dory2","jpg");
-	img2.write("imagenes/nemo2","jpg");
+
+
+	/**
+	 *  CASOS FINALES
+	 */
+
+		//lectura
+	Image dory;
+	dory.read("imagenes/dory.jpg");
+
+	Image nemo;
+	nemo.read("imagenes/nemo.jpg");
+
+		//recorrido
+	make_dfs(dory, 31);
+	make_bfs(nemo, 60);
+
+		//escritura
+	dory.write("imagenes/dory2","jpg");
+	nemo.write("imagenes/nemo2","jpg");
+
 
 	cout<<"Proceso terminado."<<endl;
 
