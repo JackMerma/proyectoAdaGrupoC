5cd24b9ae78662292aeea2dfaec53853a4c00370
Jackson Merma <jmermap@unsa.edu.pe>
2021-11-25 20:41:23 -0500

    sol archivo

---
+++ Examen_2/answer.cpp
@@ -0,0 +1,30 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @created     : 25/11/2021
+* @filename    : answer
+* @description : _
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define endl "\n"
+#define PI 3.1415926535897932384626
+
+void init_code(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	init_code();
+	
+
+	return 0;
+}
+
